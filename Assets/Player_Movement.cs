bdb8b950a3205545dc0834944b0f9184b0c8495a
godofnanners <casper.martensson@telia.com>
2021-07-13 21:00:53 +0200

    Player movement
    Implemented jumping and side movement with Rigidbody forces

---
+++ Assets/Player_Movement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    [SerializeField]
+    private float myJumpForce = 10f;
+    [SerializeField]
+    private float myDirectionForce = 10f;
+
+    Rigidbody2D myRigidbody;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            myRigidbody.AddForce(new Vector2(-myDirectionForce, 0), ForceMode2D.Force);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            myRigidbody.AddForce(new Vector2(myDirectionForce, 0), ForceMode2D.Force);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.AddForce(new Vector2(0, myJumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+}
