50471f27c80673c4120940f5e28f6203f793cd82
Oskar Therén <oskartheren@gmail.com>
2021-07-28 17:02:03 +0200

    Make sword transparent when not used and filled when used
    Filled for 20 frames after the click.

--- Assets/SwordRotation.cs
+++ Assets/SwordRotation.cs
@@ -8,6 +8,15 @@ public class SwordRotation : MonoBehaviour
     private float mydistance = 1.5f;
 
     private readonly List<GameObject> collidingObjects = new List<GameObject>();
+    private readonly int colorFillDelay = 20;
+
+    private int currentColorFillDelay = 0;
+    private Material material;
+
+    void Start()
+    {
+        material = gameObject.GetComponent<Renderer>().material;
+    }
 
     void Update()
     {
@@ -31,13 +40,27 @@ public class SwordRotation : MonoBehaviour
         {
             collidingObjects.Select(o => o.GetComponent<Health>()).Where(h => h != null).ToList()
                 .ForEach(h => h.TakeDamage(100));
+
+            ChangeAlpha(1.0f);
+            currentColorFillDelay = colorFillDelay;
         }
+        else if (currentColorFillDelay-- == 0)
+        {
+            ChangeAlpha(0.5f);
+        }
+    }
+
+    void ChangeAlpha(float alphaVal)
+    {
+        material.SetColor("_Color", new Color(material.color.r, material.color.g, material.color.b, alphaVal));
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         collidingObjects.Add(collision.gameObject);
     }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         collidingObjects.Remove(collision.gameObject);
